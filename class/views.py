34ddabee881de4619f506a593a18930df6f00afd
Aliaksandr Shyrokau <aliaksandrs@google.com>
2019-05-09 13:35:22 +0300

    created first database table Post for our app using Django models.

--- class/views.py
+++ class/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Shirokov',
-        'title': 'Swift method',
-        'content': 'Nullam maximus tellus vel dapibus iaculis. Aenean feugiat libero nec ipsum venenatis elementum. Integer at dapibus massa. Morbi non auctor sapien. In turpis nisl, volutpat in cursus quis, pretium sed nibh. Nulla ornare, quam et varius dictum, est ligula ornare neque, et hendrerit lacus est eu mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin aliquet ',
-        'date_posted': 'April 22, 2019'
-    },
-    {
-        'author': 'Smith',
-        'title': 'Smooth method',
-        'content': 'Nullam maximus tellus vel dapibus iaculis. Aenean feugiat libero nec ipsum venenatis elementum. Integer at dapibus massa. Morbi non auctor sapien. In turpis nisl, volutpat in cursus quis, pretium sed nibh. Nulla ornare, quam et varius dictum, est ligula ornare neque, et hendrerit lacus est eu mi. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Proin aliquet ',
-        'date_posted': 'April 23, 2019'
-    }
-]
+from .models import Post
 
 
 def home(request):
@@ -26,6 +12,6 @@ def about(request):
 
 def blog(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'class/blog.html', context)
